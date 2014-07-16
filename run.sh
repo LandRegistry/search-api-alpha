5a1b1c40a37a5a43f2b253ca0cb6b46584f89224
tsangsimon <simon.tsang@gmail.com>
2014-07-16 17:23:52 +0100

    fixing issue with run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 export SETTINGS='config.DevelopmentConfig'
 export SEARCH_API_URL='http://localhost:8003'
-export ES_URI = os.environ.get('http://localhost:9200')
+export ES_URI='http://localhost:9200'
 
 python run_dev.py
