105f7b26c0999c596de2d9f1f98fe934ac7f126e
tsangsimon <simon.tsang@gmail.com>
2014-07-16 15:09:11 +0100

    adding ES_URI

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export SETTINGS='config.DevelopmentConfig'
-export SEARCH  _API_URL='http://localhost:8003'
+export SEARCH_API_URL='http://localhost:8003'
+export ES_URI = os.environ.get('http://localhost:9200')
 
 python run_dev.py
