4aca13f9a48ff69a53ab611fe96a21fd8429f567
Simon Chapman <sdchapman@gmail.com>
2014-07-21 11:10:12 +0100

    Not trusting config inheritance...

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ class DevelopmentConfig(Config):
 
 class TestConfig(DevelopmentConfig):
     TESTING = True
+    ELASTICSEARCH_HOST = os.environ.get('ELASTICSEARCH_HOST')
+    ELASTICSEARCH_PORT = os.environ.get('ELASTICSEARCH_PORT')
