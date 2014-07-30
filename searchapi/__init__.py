62fcd68d0679e415430aa5c08ae1c887db79325f
tsangsimon <simon.tsang@gmail.com>
2014-07-30 16:41:26 +0100

    fixing alan changes

--- searchapi/__init__.py
+++ searchapi/__init__.py
@@ -10,7 +10,7 @@ app.logger.info("\nConfiguration\n%s\n" % app.config)
 
 # Sentry exception reporting
 if 'SENTRY_DSN' in os.environ:
-    sentry = Sentry(app, dsn=os.environ['SENTRY_DSN']
+    sentry = Sentry(app, dsn=os.environ['SENTRY_DSN'])
 
 if not app.debug:
     app.logger.addHandler(logging.StreamHandler())
