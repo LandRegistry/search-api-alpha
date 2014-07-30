a94adbd7a58f625da3e259f0f9fea6ae0da077d0
alanhugheslandregistry <alanhugheslandregistry@gmail.com>
2014-07-30 15:57:20 +0100

    adding sentry config

--- searchapi/__init__.py
+++ searchapi/__init__.py
@@ -1,11 +1,16 @@
 import os, logging
 from flask import Flask
+from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ.get('SETTINGS'))
 
 app.logger.info("\nConfiguration\n%s\n" % app.config)
+
+# Sentry exception reporting
+if 'SENTRY_DSN' in os.environ:
+    sentry = Sentry(app, dsn=os.environ['SENTRY_DSN']
 
 if not app.debug:
     app.logger.addHandler(logging.StreamHandler())
