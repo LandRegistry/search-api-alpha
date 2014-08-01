62e48baabe8698ce75b3909d2a29c3961eafc06d
Adam Shimali <adam.shimali@gmail.com>
2014-08-01 10:45:57 +0100

    Move env vars to environment and environment_test

--- config.py
+++ config.py
@@ -2,26 +2,22 @@ import os
 
 class Config(object):
     DEBUG = False
-    ELASTICSEARCH_HOST = os.environ.get('ELASTICSEARCH_HOST')
-    ELASTICSEARCH_PORT = os.environ.get('ELASTICSEARCH_PORT')
-    ELASTICSEARCH_USESSL = os.environ.get('ELASTICSEARCH_USESSL')
-    ELASTICSEARCH_USERPASS = os.environ.get('ELASTICSEARCH_USERPASS')
+    ELASTICSEARCH_HOST = os.environ['ELASTICSEARCH_HOST']
+    ELASTICSEARCH_PORT = os.environ['ELASTICSEARCH_PORT']
+    ELASTICSEARCH_USESSL = os.environ['ELASTICSEARCH_USESSL']
+    ELASTICSEARCH_USERPASS = os.environ['ELASTICSEARCH_USERPASS']
 
 class DevelopmentConfig(Config):
     DEBUG = True
 
 class TestConfig(DevelopmentConfig):
     TESTING = True
-    ELASTICSEARCH_HOST = 'localhost'
-    ELASTICSEARCH_PORT = '9200'
-    ELASTICSEARCH_USESSL = ''
-    ELASTICSEARCH_USERPASS = ''
 
 class DockerConfig(object):
     DEBUG = True
     ELASTICSEARCH_HOST = os.environ.get('ELASTICSEARCH_1_PORT_9200_TCP_ADDR')
     ELASTICSEARCH_PORT = os.environ.get('ELASTICSEARCH_1_PORT_9200_TCP_PORT')
     ELASTICSEARCH_USESSL = ''
-    ELASTICSEARCH_USERPASS = '' 
+    ELASTICSEARCH_USERPASS = ''
 
 
