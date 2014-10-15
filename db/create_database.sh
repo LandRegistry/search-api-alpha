d01ead59d08377f4b2e4bd33bd33fd9feb2a2640
Juan Matthys Uys <opyate@gmail.com>
2014-10-15 11:56:49 +0100

    use standard analyzer for postcode search

--- db/create_database.sh
+++ db/create_database.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-MAPLOC="$(dirname $0)"
 HOST=${1:-"localhost:9200"}
-curl -XPUT http://$HOST/public_titles -d @${MAPLOC}/mapping.json
+curl -XPUT http://$HOST/public_titles
 
-curl -XPUT http://$HOST/authenticated_titles -d @${MAPLOC}/mapping.json
+curl -XPUT http://$HOST/authenticated_titles
 
