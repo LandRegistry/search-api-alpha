3639d382dae36f7e81f0de767afc4ca1e686c5e9
Juan Matthys Uys <opyate@gmail.com>
2014-10-17 17:01:29 +0100

    *.entry_date as string

--- db/create_database.sh
+++ db/create_database.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+MAPLOC="$(dirname $0)"
 HOST=${1:-"localhost:9200"}
 curl -XPUT http://$HOST/public_titles
 
 curl -XPUT http://$HOST/authenticated_titles
+
+curl -XPUT http://$HOST/_all/_mapping/titles -d @${MAPLOC}/mapping.json
 
