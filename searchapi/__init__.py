b0c7e61178a5eb81dc24aab9911baaeccda546ff
Adam Shimali <adam.shimali@gmail.com>
2014-10-31 13:38:41 +0000

    Add proxy fix as in lr this will run with reverse proxy

--- searchapi/__init__.py
+++ searchapi/__init__.py
@@ -8,6 +8,9 @@ app.config.from_object(os.environ.get('SETTINGS'))
 
 app.logger.debug("\nConfiguration\n%s\n" % app.config)
 
+from werkzeug.contrib.fixers import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 # Sentry exception reporting
 if 'SENTRY_DSN' in os.environ:
     sentry = Sentry(app, dsn=os.environ['SENTRY_DSN'])
